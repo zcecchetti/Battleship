21fb86b95a7a6e0b9659ffd644e79c98d5b5f41f
Zachary Cecchetti <zcecchetti@gmail.com>
2022-06-19 23:20:44 -0700

    Began writing tests and code

--- src/components.js
+++ src/components.js
@@ -1,8 +1,34 @@
+/* eslint-disable no-undef */
+/* eslint-disable no-unreachable-loop */
+/* eslint-disable no-plusplus */
+/* eslint-disable func-names */
 /* eslint-disable import/prefer-default-export */
 
-function hellothere() {
-  const general = 'kenobi';
-  return general;
+function createShip(length) {
+  const shipArray = [];
+  for (let i = 0; i < length; i++) {
+    shipArray.push('0');
+  }
+
+  const returnArray = () => shipArray;
+
+  const hit = function (index) {
+    shipArray[index] = 'h';
+    return shipArray;
+  };
+
+  const isSunk = function () {
+    for (let i = 0; i < length; i++) {
+      if (shipArray[i] === '0') {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  return { returnArray, hit, isSunk };
 }
 
-export { hellothere };
+const smallShip = createShip(3);
+
+export { smallShip };
--- tests/components.test.js
+++ tests/components.test.js
@@ -1,7 +1,21 @@
 /* eslint-disable no-undef */
 
-import { hellothere } from '../src/components';
+import { smallShip } from '../src/components';
 
-test('Says the master jedis name', () => {
-  expect(hellothere()).toBe('kenobi');
+test('Calls createShip and returns ship array', () => {
+  expect(smallShip.returnArray()).toEqual(['0', '0', '0']);
+});
+
+test('Calls hit method and returns ship array with hit marker in correct location', () => {
+  expect(smallShip.hit(2)).toEqual(['0', '0', 'h']);
+});
+
+test('Calls isSunk and fails because ship has unmarked spaces', () => {
+  expect(smallShip.isSunk()).toBe(false);
+});
+
+test('Calls isSunk and fails because shipArray is full of hit markers', () => {
+  smallShip.hit(0);
+  smallShip.hit(1);
+  expect(smallShip.isSunk()).toBe(true);
 });
